8b16bd2d7a8b53f67f5becd076d45b21e5d36791
nd <nd@hypr.sh>
2025-01-17 07:40:32 +0100

    fix: switch back to D34DC3N73R fork

--- pkgs/by-name/sn/snapraid-btrfs/package.nix
+++ pkgs/by-name/sn/snapraid-btrfs/package.nix
@@ -1,42 +1,35 @@
 {
-  lib,
-  stdenv,
+  symlinkJoin,
   fetchFromGitHub,
-  writeText,
+  writeScriptBin,
+  makeWrapper,
   coreutils,
   gnugrep,
   gawk,
   gnused,
   snapraid,
   snapper,
-  makeWrapper,
-}:
-stdenv.mkDerivation rec {
-  pname = "snapraid-btrfs";
-  version = "1.0.0";
-
-  src = fetchFromGitHub {
-    owner = "automorphism88";
-    repo = "snapraid-btrfs";
-    rev = "6492a45ad55c389c0301075dcc8bc8784ef3e274";
-    sha256 = "1xd2wqvp0dnsys6wc8ambisgmp0l3bw04pk49nivr3vvbj70myb0";
-  };
-
-  nativeBuildInputs = [makeWrapper];
-  buildInputs = [coreutils gnugrep gawk gnused snapraid snapper];
-
-  installPhase = ''
-    install -Dm755 ${src}/snapraid-btrfs $out/bin/${pname}
-    patchShebangs $out/bin/${pname}
-    wrapProgram $out/bin/${pname} \
-      --set PATH ${lib.makeBinPath [coreutils gnugrep gawk gnused snapraid snapper]}
-  '';
-
-  meta = with lib; {
-    description = "Wrapper script for integrating SnapRAID and Btrfs";
-    homepage = "https://github.com/automorphism88/snapraid-btrfs";
-    license = licenses.mit;
-    maintainers = [maintainers.nd];
-    platforms = platforms.all;
-  };
-}
+}: let
+  name = "snapraid-btrfs";
+  deps = [coreutils gnugrep gawk gnused snapraid snapper];
+  script =
+    (
+      writeScriptBin name
+      (builtins.readFile ((fetchFromGitHub {
+          owner = "D34DC3N73R";
+          repo = "snapraid-btrfs";
+          rev = "8cdbf54100c2b630ee9fcea11b14f58a894b4bf3";
+          sha256 = "IQgL55SMwViOnl3R8rQ9oGsanpFOy4esENKTwl8qsgo=";
+        })
+        + "/snapraid-btrfs"))
+    )
+    .overrideAttrs (old: {
+      buildCommand = "${old.buildCommand}\n patchShebangs $out";
+    });
+in
+  symlinkJoin {
+    inherit name;
+    paths = [script] ++ deps;
+    buildInputs = [makeWrapper];
+    postBuild = "wrapProgram $out/bin/${name} --set PATH $out/bin";
+  }
