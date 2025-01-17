8c32c765e4f2a16f03979f1f79a9d62456ea3a35
nd <nd@hypr.sh>
2025-01-17 07:50:00 +0100

    fix: convert to follow best practices

--- pkgs/by-name/sn/snapraid-btrfs/package.nix
+++ pkgs/by-name/sn/snapraid-btrfs/package.nix
@@ -1,35 +1,42 @@
 {
-  symlinkJoin,
+  lib,
+  stdenv,
   fetchFromGitHub,
-  writeScriptBin,
-  makeWrapper,
+  writeText,
   coreutils,
   gnugrep,
   gawk,
   gnused,
   snapraid,
   snapper,
-}: let
-  name = "snapraid-btrfs";
-  deps = [coreutils gnugrep gawk gnused snapraid snapper];
-  script =
-    (
-      writeScriptBin name
-      (builtins.readFile ((fetchFromGitHub {
-          owner = "D34DC3N73R";
-          repo = "snapraid-btrfs";
-          rev = "8cdbf54100c2b630ee9fcea11b14f58a894b4bf3";
-          sha256 = "IQgL55SMwViOnl3R8rQ9oGsanpFOy4esENKTwl8qsgo=";
-        })
-        + "/snapraid-btrfs"))
-    )
-    .overrideAttrs (old: {
-      buildCommand = "${old.buildCommand}\n patchShebangs $out";
-    });
-in
-  symlinkJoin {
-    inherit name;
-    paths = [script] ++ deps;
-    buildInputs = [makeWrapper];
-    postBuild = "wrapProgram $out/bin/${name} --set PATH $out/bin";
-  }
+  makeWrapper,
+}:
+stdenv.mkDerivation rec {
+  pname = "snapraid-btrfs";
+  version = "1.0.0";
+
+  src = fetchFromGitHub {
+    owner = "D34DC3N73R";
+    repo = "snapraid-btrfs";
+    rev = "8cdbf54100c2b630ee9fcea11b14f58a894b4bf3";
+    sha256 = "IQgL55SMwViOnl3R8rQ9oGsanpFOy4esENKTwl8qsgo=";
+  };
+
+  nativeBuildInputs = [makeWrapper];
+  buildInputs = [coreutils gnugrep gawk gnused snapraid snapper];
+
+  installPhase = ''
+    install -Dm755 ${src}/snapraid-btrfs $out/bin/${pname}
+    patchShebangs $out/bin/${pname}
+    wrapProgram $out/bin/${pname} \
+      --set PATH ${lib.makeBinPath [coreutils gnugrep gawk gnused snapraid snapper]}
+  '';
+
+  meta = with lib; {
+    description = "Wrapper script for integrating SnapRAID and Btrfs";
+    homepage = "https://github.com/automorphism88/snapraid-btrfs";
+    license = licenses.mit;
+    maintainers = [maintainers.nd];
+    platforms = platforms.all;
+  };
+}
